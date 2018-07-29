006ee872cf1800ead7469420ed77d896fb21c035
Marlon Monzon <martianworm17@gmail.com>
2018-07-29 12:12:55 +0800

    Add timetable.sh shell file

---
+++ timetable.sh
@@ -0,0 +1,5 @@
+# PNR Timetable
+# I've noticed that I don't have time to create the javascript / json / html5 version yet.
+# So I'll just try to create it on shell script
+# This will serve as my practice also in basic shell scripting
+
